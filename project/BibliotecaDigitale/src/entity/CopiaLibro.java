fa06289f3def8cd80788454e074a1bbc3cd69888
Matteo Bottari <ma.bottari@studenti.unina.it>
2025-07-06 21:09:29 +0200

    Aggiunta metodi di set e get e controllo stato CopiaLibro

--- project/BibliotecaDigitale/src/entity/CopiaLibro.java
+++ project/BibliotecaDigitale/src/entity/CopiaLibro.java
@@ -7,9 +7,66 @@ public class CopiaLibro {
     private String stato;
     private Libro libro;
 
-    public CopiaLibro(String stato, Libro libro){
+    public CopiaLibro(Libro libro){
         this.ID = counter++;
-        this.stato = stato;
+        this.stato = "Disponibile";
         this.libro = libro;
+
+        this.libro.incrementaDisponibilitaCopie();
+    }
+
+    @Override
+    public String toString(){
+        return "CopiaLibro ID: " + this.ID + "\nStato: " + this.stato + "\nLibro: " + this.libro.getTitolo();
+    }
+
+    public int getID() {
+        return this.ID;
+    }
+
+    public String getStato(){
+        return this.stato;
+    }
+
+    public Libro getLibro(){
+        return this.libro;
+    }
+
+    public void setStatoPrenotato(){
+        this.stato = "Prenotato";
+        this.libro.decrementaDisponibilitaCopie();
+    }
+
+    public void setStatoInPrestito(){
+        this.stato = "In prestito";
+    }
+
+    public void setStatoDisponibile(){
+        this.stato = "Disponibile";
+    }
+
+    /* 
+    public boolean checkStato() {
+        String statoUpper = this.stato.toUpperCase();
+    
+        if (statoUpper.equals("PRENOTATO") || 
+            statoUpper.equals("IN PRESTITO") || 
+            statoUpper.equals("DISPONIBILE")) {
+            return true;
+        }
+    
+        return false;
+    } */
+
+    public boolean isDisponibile(){
+        return this.stato.equals("Disponibile");
+    }
+
+    public boolean isPrenotato(){
+        return this.stato.equals("Prenotato");
+    }
+
+    public boolean isInPrestito(){
+        return this.stato.equals("In prestito");
     }
 }
