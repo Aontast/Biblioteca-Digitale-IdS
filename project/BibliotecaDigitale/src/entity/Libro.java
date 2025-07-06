b46a87a333862e4e6879bafda45c96c164c4fc3d
Antonio <digiorgio.antonio18@gmail.com>
2025-07-06 20:47:53 +0200

    creazione del progetto tramite Intellij, sono stati creati i package di partenza

--- project/BibliotecaDigitale/src/entity/Libro.java
+++ project/BibliotecaDigitale/src/entity/Libro.java
@@ -1,4 +1,29 @@
 package entity;
 
 public class Libro {
+
+    private String titolo;
+    private String autore;
+    private int annoDiPubblicazione;
+    private String genere;
+    private int codiceISBN;
+    private String descrizione;
+
+    private static int disponibilitaCopie;
+    private Copia copia[];
+
+    public Libro(String titolo, String autore, int annoDiPubblicazione, String genere, int codiceISBN, String descrizione) {
+        this.titolo = titolo;
+        this.autore = autore;
+        this.annoDiPubblicazione = annoDiPubblicazione;
+        this.genere = genere;
+        this.codiceISBN = codiceISBN;
+        this.descrizione = descrizione;
+    }
+
+    public void aggiungiCopia() {
+        Copia copia = new Copia();
+        copia[disponibilitaCopie] = copia;
+        disponibilitaCopie++;
+    }
 }
